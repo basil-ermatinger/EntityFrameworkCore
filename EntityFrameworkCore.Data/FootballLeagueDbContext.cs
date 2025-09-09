ecaa43082c12d6a8045d626fe913666a9fab33eb
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-09 19:53:57 +0200

    Added database context and some models

---
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data
+{
+	public class FootballLeagueDbContext : DbContext
+	{
+		public DbSet<Team> Teams { get; set; }
+		public DbSet<Coach> Coaches { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt=False");
+		}
+	}
+}
---
+++ EntityFrameworkCore.Domain/Coach.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+	public class Coach : BaseDomainModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+ 
