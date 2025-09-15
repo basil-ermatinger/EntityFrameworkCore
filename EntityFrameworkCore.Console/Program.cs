d1903ac3034e577cc5efbb35707df9769fef5fe5
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-15 10:39:45 +0200

    Implemented queries for selecting a single record.

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,12 +1,34 @@
 ﻿using EntityFrameworkCore.Data;
 using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
 
 // First we need instance of context
-FootballLeagueDbContext context = new FootballLeagueDbContext();
+using FootballLeagueDbContext context = new FootballLeagueDbContext();
 
 // Select all teams
-// SELECT * FROM TEAMS
+GetAllTeams();
 
-List<Team> teams = context.Teams.ToList();
+// Selecting a single record - First one in the list
+Team? teamOne = await context.Teams.FirstOrDefaultAsync();
 
-teams.ForEach(t => Console.WriteLine(t.Name));
+// Selecting a single record - First one in the list that meets a condition
+Team? teamTwo = await context.Teams.FirstOrDefaultAsync(t => t.TeamId == 1);
+
+// Selecting a single record - Only one record should be returned
+Team? teamThree = await context.Teams.SingleOrDefaultAsync(t => t.TeamId == 2);
+
+// Selecting based on Id
+Team? teamBasedOnId = await context.Teams.FindAsync(3);
+
+if(teamBasedOnId != null)
+{
+	Console.WriteLine(teamBasedOnId.Name);
+}
+
+void GetAllTeams()
+{
+	// SELECT * FROM TEAMS
+	List<Team> teams = context.Teams.ToList();
+
+	teams.ForEach(t => Console.WriteLine(t.Name));
+}
